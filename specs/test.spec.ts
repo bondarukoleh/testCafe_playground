444e59fb005347bec63a48497adabbf693c3f9dd
Oleh Bondaruk <oleh.bondaruk@payconiq.com>
2020-11-05 14:00:45 +0100

    added a few lines

--- specs/test.spec.ts
+++ specs/test.spec.ts
@@ -1,11 +1,16 @@
 import {Selector} from "testcafe";
 const testCafeExampleURL = `https://devexpress.github.io/testcafe/example/`
 
+const nameInput = Selector('#developer-name');
+const osOption = Selector('#macos');
+const submit = Selector('#submit-button');
+
 fixture `First fixture`.page`${testCafeExampleURL}`
 
-test(`Reach the example page`, async controller => {
-  const nameInput = await Selector('#developer-name');
-
-  await controller.typeText(nameInput, 'Hello Oleh');
-  await controller.wait(5000);
+test.page(`https://devexpress.github.io/testcafe/example/`)
+(`Reach the example page`, async controller => {
+  await controller
+    .typeText(nameInput, 'Hello Oleh')
+    .click(osOption)
+    .click(submit);
 })
