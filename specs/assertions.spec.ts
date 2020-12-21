25d92fa316475425c7cc146863b8fa86fee1b265
Oleh Bondaruk <bondaruk9000@gmail.com>
2020-12-21 18:04:02 +0100

    added some info

--- specs/assertions.spec.ts
+++ specs/assertions.spec.ts
@@ -5,6 +5,7 @@ fixture `Assertion fixture` .page `${testCafeExampleURL}`
 
 test(`Test 1`, async t => {
   await t
+    .setTestSpeed(0.5)
     .expect(Selector('#something').innerText).eql('sometext')
     .expect({a: 1}).ok({timeout: 1000}) // truthy
     .expect(Selector('#something').value).eql('')
